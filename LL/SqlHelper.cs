7573107211d6c64fba6bf3fa09220405c837b1b1
Kim <codercn@qq.com>
2016-07-01 10:48:38 +0800

    添加connetDatabase函数

---
+++ LL/SqlHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Collections;
+
+
+namespace SqlHelper
+{
+    public static class SqlHeper
+    {
+        static OleDbConnection ConnectDatabase ()
+        {
+            OleDbConnection conn;
+            conn = new OleDbConnection(@"Provider='Microsoft.Jet.OLEDB.4.0';Data Source='"  + @"/para.accdb'");
+            return conn;
+        }
+
+    }
+
+}
