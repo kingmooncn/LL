c388ae054cdd921aa1523407c31370ac6be5a779
Kim <codercn@qq.com>
2016-07-01 10:35:50 +0800

    修改界面

--- LL/Form1.Designer.cs
+++ LL/Form1.Designer.cs
@@ -28,21 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tcMain = new System.Windows.Forms.TabControl();
+            this.bpParameter = new System.Windows.Forms.TabPage();
+            this.tpPM = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tcMain.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tcMain
+            // 
+            this.tcMain.Controls.Add(this.bpParameter);
+            this.tcMain.Controls.Add(this.tpPM);
+            this.tcMain.Location = new System.Drawing.Point(1, 32);
+            this.tcMain.Name = "tcMain";
+            this.tcMain.SelectedIndex = 0;
+            this.tcMain.Size = new System.Drawing.Size(933, 504);
+            this.tcMain.TabIndex = 0;
+            // 
+            // bpParameter
+            // 
+            this.bpParameter.Location = new System.Drawing.Point(4, 22);
+            this.bpParameter.Name = "bpParameter";
+            this.bpParameter.Padding = new System.Windows.Forms.Padding(3);
+            this.bpParameter.Size = new System.Drawing.Size(925, 478);
+            this.bpParameter.TabIndex = 0;
+            this.bpParameter.Text = "参数";
+            this.bpParameter.UseVisualStyleBackColor = true;
+            // 
+            // tpPM
+            // 
+            this.tpPM.Location = new System.Drawing.Point(4, 22);
+            this.tpPM.Name = "tpPM";
+            this.tpPM.Padding = new System.Windows.Forms.Padding(3);
+            this.tpPM.Size = new System.Drawing.Size(903, 368);
+            this.tpPM.TabIndex = 1;
+            this.tpPM.Text = "PM";
+            this.tpPM.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.编辑ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(934, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 编辑ToolStripMenuItem
+            // 
+            this.编辑ToolStripMenuItem.Name = "编辑ToolStripMenuItem";
+            this.编辑ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.编辑ToolStripMenuItem.Text = "编辑";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(934, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(608, 303);
+            this.ClientSize = new System.Drawing.Size(934, 561);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tcMain);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "LL";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tcMain.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tcMain;
+        private System.Windows.Forms.TabPage bpParameter;
+        private System.Windows.Forms.TabPage tpPM;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 编辑ToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
     }
 }
 
--- LL/Form1.cs
+++ LL/Form1.cs
@@ -19,8 +19,7 @@ namespace LL
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            int a = 1;
-            int b = 2;
+
         }
     }
 }
